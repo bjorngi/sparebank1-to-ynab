8d46cce3c1849803d6b2b0a3b7b0e7308e7565e6
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-04-17 21:45:13 +0200

    Refactor YNAB module to use client struct

--- src/ynab.rs
+++ src/ynab.rs
@@ -32,64 +32,10 @@ pub struct CreateYnabTransactionResponseData {
     pub duplicate_import_ids: Vec<String>,
 }
 
-fn parse_transactions(
-    transactions: Vec<sparebanken1::Transaction>,
-    account_config: &HashMap<String, String>,
-) -> Vec<CreateYnabTransaction> {
-    transactions
-        .iter()
-        .scan(Vec::new(), |state, t| {
-            let oslo_time = t.date.with_timezone(&Oslo);
-            let formated_date = oslo_time.format("%Y-%m-%d").to_string();
-            let account_id = account_config.get(&t.account).unwrap();
-
-            // Check if same transactions has been imported before
-            let import_prefix = format!("SB1:{}:{}", t.amount, formated_date);
-            state.push(import_prefix.clone());
-            let import_count = state
-                .iter()
-                .filter(|id| id.starts_with(&import_prefix))
-                .count();
-            let import_id = format!("{}:{}", import_prefix, import_count);
-
-            Some(CreateYnabTransaction {
-                date: formated_date,
-                account_id: account_id.clone(),
-                amount: (t.amount * 1000.0) as i64,
-                payee_name: t.payee.clone(),
-                cleared: String::from("cleared"),
-                memo: t.description.clone(),
-                import_id,
-            })
-        })
-        .collect()
-}
-
-pub async fn add_transactions(
-    ynab_access_token: &str,
-    ynab_budget_id: &str,
+pub struct YnabClient {
+    ynab_token: String,
+    ynab_budget: String,
     account_config: HashMap<String, String>,
-    transactions: Vec<sparebanken1::Transaction>,
-) -> Result<CreateYnabTransactionResponseData, reqwest::Error> {
-    let url: &str = &format!("{}/budgets/{}/transactions", BASE_API_URL, ynab_budget_id);
-    let ynab_transactions: Vec<CreateYnabTransaction> =
-        parse_transactions(transactions, &account_config);
-
-    let data = CreateYnabTransactionRequest {
-        transactions: ynab_transactions,
-    };
-
-    let client = reqwest::Client::new();
-    let response = client
-        .post(url)
-        .header("Authorization", &format!("Bearer {}", ynab_access_token))
-        .json(&data)
-        .send()
-        .await?
-        .json::<CreateYnabTransactionResponse>()
-        .await?;
-
-    Ok(response.data)
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -98,6 +44,7 @@ pub struct Account {
     pub name: String,
     closed: bool,
 }
+
 #[derive(Debug, Deserialize)]
 struct YnabAccountResponse {
     accounts: Vec<Account>,
@@ -106,33 +53,6 @@ struct YnabAccountResponse {
 #[derive(Debug, Deserialize)]
 struct YnabAccountsResponse {
     data: YnabAccountResponse,
-}
-
-pub async fn get_accounts(
-    ynab_access_token: &str,
-    ynab_budget_id: &str,
-) -> Result<Vec<Account>, reqwest::Error> {
-    let url = format!("{BASE_API_URL}/budgets/{}/accounts", ynab_budget_id);
-
-    let response = reqwest::Client::new()
-        .get(url)
-        .header("Authorization", &format!("Bearer {}", ynab_access_token))
-        .send()
-        .await
-        .expect("Failed to get account from YNAB")
-        .json::<YnabAccountsResponse>()
-        .await
-        .expect("Could not parse data");
-
-    let filteredAccounts = response
-        .data
-        .accounts
-        .iter()
-        .filter(|&acc| !acc.closed)
-        .cloned()
-        .collect();
-
-    Ok(filteredAccounts)
 }
 
 #[derive(Debug, Deserialize)]
@@ -150,15 +70,111 @@ struct YnabBudgetsResponse {
 struct YnabBudgetsDataResponse {
     data: YnabBudgetsResponse,
 }
-pub async fn get_budgets(ynab_access_token: &str) -> Result<Vec<Budget>, reqwest::Error> {
-    let url = format!("{BASE_API_URL}/budgets/");
-    let response = reqwest::Client::new()
-        .get(url)
-        .header("Authorization", &format!("Bearer {}", ynab_access_token))
-        .send()
-        .await?
-        .json::<YnabBudgetsDataResponse>()
-        .await?;
 
-    Ok(response.data.budgets)
+impl YnabClient {
+    pub fn new(
+        account_config: HashMap<String, String>,
+        ynab_token: String,
+        ynab_budget: String,
+    ) -> Self {
+        YnabClient {
+            ynab_token,
+            ynab_budget,
+            account_config,
+        }
+    }
+
+    fn parse_transactions(
+        &self,
+        transactions: &[sparebanken1::Transaction],
+    ) -> Vec<CreateYnabTransaction> {
+        transactions
+            .iter()
+            .scan(Vec::new(), |state, t| {
+                let oslo_time = t.date.with_timezone(&Oslo);
+                let formated_date = oslo_time.format("%Y-%m-%d").to_string();
+                let account_id = self.account_config.get(&t.account).unwrap();
+
+                // Check if same transactions has been imported before
+                let import_prefix = format!("SB1:{}:{}", t.amount, formated_date);
+                state.push(import_prefix.clone());
+                let import_count = state
+                    .iter()
+                    .filter(|id| id.starts_with(&import_prefix))
+                    .count();
+                let import_id = format!("{}:{}", import_prefix, import_count);
+
+                Some(CreateYnabTransaction {
+                    date: formated_date,
+                    account_id: account_id.clone(),
+                    amount: (t.amount * 1000.0) as i64,
+                    payee_name: t.payee.clone(),
+                    cleared: String::from("cleared"),
+                    memo: t.description.clone(),
+                    import_id,
+                })
+            })
+            .collect()
+    }
+
+    pub async fn add_transactions(
+        &self,
+        transactions: Vec<sparebanken1::Transaction>,
+    ) -> Result<CreateYnabTransactionResponseData, reqwest::Error> {
+        let url = format!("{}/budgets/{}/transactions", BASE_API_URL, self.ynab_budget);
+        let ynab_transactions = self.parse_transactions(&transactions);
+
+        let data = CreateYnabTransactionRequest {
+            transactions: ynab_transactions,
+        };
+
+        let client = reqwest::Client::new();
+        let response = client
+            .post(url)
+            .header("Authorization", &format!("Bearer {}", self.ynab_token))
+            .json(&data)
+            .send()
+            .await?
+            .json::<CreateYnabTransactionResponse>()
+            .await?;
+
+        Ok(response.data)
+    }
+
+    pub async fn get_accounts(&self) -> Result<Vec<Account>, reqwest::Error> {
+        let url = format!("{BASE_API_URL}/budgets/{}/accounts", self.ynab_budget);
+
+        let response = reqwest::Client::new()
+            .get(url)
+            .header("Authorization", &format!("Bearer {}", self.ynab_token))
+            .send()
+            .await
+            .expect("Failed to get account from YNAB")
+            .json::<YnabAccountsResponse>()
+            .await
+            .expect("Could not parse data");
+
+        let filtered_accounts = response
+            .data
+            .accounts
+            .iter()
+            .filter(|&acc| !acc.closed)
+            .cloned()
+            .collect();
+
+        Ok(filtered_accounts)
+    }
+
+    pub async fn get_budgets(&self) -> Result<Vec<Budget>, reqwest::Error> {
+        let url = format!("{BASE_API_URL}/budgets/");
+        let response = reqwest::Client::new()
+            .get(url)
+            .header("Authorization", &format!("Bearer {}", self.ynab_token))
+            .send()
+            .await?
+            .json::<YnabBudgetsDataResponse>()
+            .await?;
+
+        Ok(response.data.budgets)
+    }
 }
